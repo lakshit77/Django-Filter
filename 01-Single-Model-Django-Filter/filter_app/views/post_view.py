1b9ed5fec21fe580f304cb677667222a266776cd
Lakshit Ukani <lakshitukani@Admins-MacBook-Pro.local>
2023-04-10 01:15:11 +0530

    commit added

---
+++ 01-Single-Model-Django-Filter/filter_app/views/post_view.py
@@ -0,0 +1,13 @@
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import viewsets
+from filter_app.models import Post
+from filter_app.serializers import PostSerializer
+from rest_framework import filters
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    filter_backends = [DjangoFilterBackend, filters.OrderingFilter, filters.SearchFilter]
+    filterset_fields = ['title']
+    search_fields = ["title"]
+    ordering_fields = "__all__"
