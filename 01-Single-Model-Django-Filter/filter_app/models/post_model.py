eeb64c9d214b5591d0040cde617949620041b2fe
Lakshit Ukani <lakshitukani@Admins-MacBook-Pro.local>
2023-05-02 23:57:17 +0530

    filter added

--- 01-Single-Model-Django-Filter/filter_app/models/post_model.py
+++ 01-Single-Model-Django-Filter/filter_app/models/post_model.py
@@ -5,6 +5,7 @@ from django.db import models
 class Post(models.Model):
     title = models.CharField(max_length=255)
     content = models.TextField()
+    age = models.IntegerField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
--- 01-Single-Model-Django-Filter/filter_app/views/post_view.py
+++ 01-Single-Model-Django-Filter/filter_app/views/post_view.py
@@ -3,11 +3,24 @@ from rest_framework import viewsets
 from filter_app.models import Post
 from filter_app.serializers import PostSerializer
 from rest_framework import filters
+from django_filters import rest_framework as filter
+
+
+class PostFilter(filter.FilterSet):
+    somethinggg = filter.CharFilter(field_name="title", lookup_expr="iexact")
+    age = filter.NumberFilter(field_name="age", lookup_expr="gt")
 
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
-    filter_backends = [DjangoFilterBackend, filters.OrderingFilter]
-    filterset_fields = ["title"]
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+    # filterset_fields = ["title"]
+    filterset_class = PostFilter
+    search_fields = ["title", "content"]
     ordering_fields = "__all__"
+
+
+
+
+    
     
